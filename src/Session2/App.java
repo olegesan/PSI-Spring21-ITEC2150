0f2c16dc22a3af2512a517525ab94637f1473dd3
Oleg <obazylnikov@ggc.edu>
2021-02-02 13:49:58 -0500

    Adds session 1 task and solution.

---
+++ src/Session2/App.java
@@ -0,0 +1,10 @@
+package Session2;
+
+public class App {
+
+    private String name;
+    public App(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/Session2/PhoneTester.java
@@ -0,0 +1,9 @@
+package Session2;
+
+public class PhoneTester {
+    public static void main(String[] args) {
+        Phone cellPhone1 = new Phone("iPhone", "7", "iOS");
+        cellPhone1.call();
+        System.out.println(cellPhone1);
+    }
+}
