dc776235d3f3d1f4039f7d1ce1c0e07658bec1b9
Oleg <obazylnikov@ggc.edu>
2021-02-09 11:12:03 -0500

    Updates session 3 starter code. Adds comments and TODOS

--- src/Session3/StartHere/App.java
+++ src/Session3/StartHere/App.java
@@ -3,10 +3,14 @@ package Session3.StartHere;
 public class App {
 
     private String name;
+    //TODO: add size and developer attributes
+
+    //TODO: update @App constructor to use new attributes
     public App(String name) {
         this.name = name;
     }
 
+    //TODO: update @toString method to depict new attributes
     @Override
     public String toString() {
         return "App{" +
--- src/Session3/StartHere/PhoneTester.java
+++ src/Session3/StartHere/PhoneTester.java
@@ -2,6 +2,9 @@ package Session3.StartHere;
 
 public class PhoneTester {
     public static void main(String[] args) {
+        /*
+        Old code. Delete or do not edit without need.
+         */
         Phone cellPhone1 = new Phone("iPhone", "7", "iOS");
         cellPhone1.call();
         System.out.println(cellPhone1);
@@ -13,5 +16,16 @@ public class PhoneTester {
         cellPhone1.install(robinhood);
 
         System.out.println(cellPhone1);
+        /*
+        End of old code. Write your code below this line
+         */
+
+        // TODO: create a new phone object
+
+        // TODO: create two new app objects
+
+        // TODO: install apps on the phone object you've created
+
+        // TODO: print the phone object
     }
 }
