d08659b9f00669e36fe3c3e4797520b2c96672bd
Oleg <obazylnikov@ggc.edu>
2021-02-10 18:03:47 -0500

    Updates session 3 starter code. Adds completed code and completes it with previously not commited files.

---
+++ src/Session3/Completed/PhoneTester.java
@@ -0,0 +1,63 @@
+package Session3.Completed;
+
+public class PhoneTester {
+    public static void main(String[] args) {
+        Phone cellPhone1 = new Phone("iPhone", "7", "iOS");
+        cellPhone1.call();
+        System.out.println(cellPhone1);
+
+        App tiktok = new App("Tiktok", "TikTok Pte. ltd.", 383.8);
+        App robinhood = new App("Robinhood", "Robinhood Markets, Inc.", 230.4);
+
+        cellPhone1.install(tiktok);
+        cellPhone1.install(robinhood);
+
+
+        OnePLus6 onePLus = new OnePLus6();
+        System.out.println(onePLus);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println(cellPhone1);
+
+        Phone pixel = new Pixel("4a 5G");
+        App nrc = new NikeRunningClub();
+        App clickUp = new ClickUp();
+        pixel.install(nrc);
+        pixel.install(clickUp);
+        System.out.println(pixel);
+    }
+}
---
+++ src/Session3/Completed/Pixel.java
@@ -0,0 +1,8 @@
+package Session3.Completed;
+
+public class Pixel extends Phone{
+
+    public Pixel(String model){
+        super("Google", model, "Android");
+    }
+}
