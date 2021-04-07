6374e6382ce53ca63826e17fdd142c07c25fcef5
Oleg <obazylnikov@ggc.edu>
2021-04-07 18:04:28 -0400

    updates session 11 problem statement

--- src/Session11/StartHere/Main.java
+++ src/Session11/StartHere/Main.java
@@ -6,6 +6,15 @@ public class Main {
         recursive(n);
     }
 
+    /*
+    Write a method that will accept a positive integer (n) and print out all the integers between n and 0, including
+    n and 0.
+    i.e. n=3 should print out the following:
+    3
+    2
+    1
+    0
+     */
     public static int recursive(int n) {
        //change lines below
         return 0;
